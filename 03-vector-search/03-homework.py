06cd1dfbd28c8102eba55189cf903be466066d8e
jankes2000 <jankes2000@interia.pl>
2024-07-10 14:05:54 +0200

    Implemented: question 3

--- 03-vector-search/03-homework.py
+++ 03-vector-search/03-homework.py
@@ -1,6 +1,19 @@
 from sentence_transformers import SentenceTransformer
 import requests 
 import numpy as np
+from vector_search_engine import VectorSearchEngine  # Import klasy
+
+
+# class VectorSearchEngine():
+#     def __init__(self, documents, embeddings):
+#         self.documents = documents
+#         self.embeddings = embeddings
+
+#     def search(self, v_query, num_results=10):
+#         scores = self.embeddings.dot(v_query)
+#         idx = np.argsort(-scores)[:num_results]
+#         return [self.documents[i] for i in idx]
+
 
 model_name = "multi-qa-distilbert-cos-v1"
 embedding_model = SentenceTransformer(model_name)
@@ -18,7 +31,7 @@ docs_url = f'{base_url}/{relative_url}?raw=1'
 docs_response = requests.get(docs_url)
 doc_raw_before_filtering = docs_response.json()
 
-documents = []
+#documents = []
 
 
 documents_raw = [doc for doc in doc_raw_before_filtering if doc['course'] == 'machine-learning-zoomcamp']
@@ -35,3 +48,14 @@ for doc in documents_raw:
 X = np.array(embeddings)
 
 print(X.shape)
+
+v = result_vector
+scores = X.dot(v)
+
+highest_score = np.max(scores)
+print(highest_score)
+
+search_engine = VectorSearchEngine(documents=documents_raw, embeddings=X)
+search_result = search_engine.search(v, num_results=5)
+
+print(search_result)
---
+++ 03-vector-search/vector_search_engine.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class VectorSearchEngine:
+    def __init__(self, documents, embeddings):
+        self.documents = documents
+        self.embeddings = embeddings
+
+    def search(self, v_query, num_results=10):
+        scores = self.embeddings.dot(v_query)
+        idx = np.argsort(-scores)[:num_results]
+        return [self.documents[i] for i in idx]
