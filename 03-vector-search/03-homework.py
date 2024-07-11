b868198ca2913c893dfbe2da21cf35af435e54a1
jankes2000 <jankes2000@interia.pl>
2024-07-11 12:44:19 +0200

    Question 5, 6 answered

--- 03-vector-search/03-homework.py
+++ 03-vector-search/03-homework.py
@@ -5,7 +5,6 @@ import pandas as pd
 from vector_search_engine import VectorSearchEngine  
 from tqdm.auto import tqdm
 from elasticsearch import Elasticsearch
-# Import klasy
 
 
 def evaluate(ground_truth, search_function):
@@ -21,16 +20,9 @@ def evaluate(ground_truth, search_function):
         'hit_rate': hit_rate(relevance_total),
     }
 
-def hit_rate(relevance_total):
-    cnt = 0
 
-    for line in relevance_total:
-        if True in line:
-            cnt = cnt + 1
 
-    return cnt / len(relevance_total)
-
-def hit_rate2(search_engine, ground_truth, num_results=5):
+def hit_rate(search_engine, ground_truth, num_results=5):
     hits = 0
     
     for item in ground_truth:
@@ -48,6 +40,39 @@ def hit_rate2(search_engine, ground_truth, num_results=5):
     
     return hits / len(ground_truth)
 
+
+def elasticsearch_search(es, index_name, query_vector, num_results=5):
+    search_query = {
+        "size": num_results,
+        "query": {
+            "script_score": {
+                "query": {"match_all": {}},
+                "script": {
+                    "source": "cosineSimilarity(params.query_vector, 'embedding') + 1.0",
+                    "params": {"query_vector": query_vector}
+                }
+            }
+        }
+    }
+    response = es.search(index=index_name, body=search_query)
+    return response['hits']['hits']
+
+def hit_rate_elasticsearch(es, index_name, embedding_model, ground_truth, num_results=5):
+    hits = 0
+    
+    for item in tqdm(ground_truth):
+        query = item['question']
+        true_document_id = str(item['document'])
+        
+        v_query = embedding_model.encode(query).tolist()
+        results = elasticsearch_search(es, index_name, v_query, num_results=num_results)
+        
+        for result in results:
+            if result['_id'] == true_document_id:
+                hits += 1
+                break
+    
+    return hits / len(ground_truth)
 
 model_name = "multi-qa-distilbert-cos-v1"
 embedding_model = SentenceTransformer(model_name)
@@ -110,8 +135,82 @@ relevance_total = []
 #     relevance = [d['id'] == doc_id for d in search_result]
 #     relevance_total.append(relevance)
 
-hr = hit_rate2(search_engine, ground_truth, num_results=5)
-print(hr)
+# hr = hit_rate(search_engine, ground_truth, num_results=5)
+# print(hr)
 
 # print(relevance_total)
 # print(hit_rate(relevance_total))
+
+es = Elasticsearch("http://localhost:9200")
+
+# Define the index settings and mappings
+index_name = "qa_embeddings"
+embedding_dim = 768  # Change this to the actual dimension of your embeddings
+
+index_settings = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0,
+        "analysis": {
+            "analyzer": {
+                "default": {
+                    "type": "standard"
+                }
+            }
+        }
+    },
+    "mappings": {
+        "properties": {
+            "question": {"type": "text"},
+            "answer": {"type": "text"},
+            "course": {"type": "keyword"},
+            "embedding": {
+                "type": "dense_vector",
+                "dims": embedding_dim
+            }
+        }
+    }
+}
+
+# Create the index
+if es.indices.exists(index=index_name):
+    es.indices.delete(index=index_name)
+es.indices.create(index=index_name, body=index_settings)
+
+for i, doc in enumerate(documents_raw):
+    embedding = embeddings[i].tolist()  # Convert embedding to list for JSON serialization
+    doc_body = {
+        "question": doc['question'],
+        "answer": doc['text'],
+        "course": doc['course'],
+        "embedding": embedding
+    }
+    es.index(index=index_name, body=doc_body, id=doc['id'])
+
+    # Define the query vector
+query_vector = result_vector.tolist()
+
+# Perform the search
+search_query = {
+    "size": 5,
+    "query": {
+        "script_score": {
+            "query": {"match_all": {}},
+            "script": {
+                "source": "cosineSimilarity(params.query_vector, 'embedding') + 1.0",
+                "params": {"query_vector": query_vector}
+            }
+        }
+    }
+}
+
+response = es.search(index=index_name, body=search_query)
+
+# Get the ID of the document with the highest score
+top_doc_id = response['hits']['hits'][0]['_id']
+print(f"The ID of the document with the highest score is: {top_doc_id}")
+
+results = elasticsearch_search(es, index_name, result_vector, num_results=5)
+index_name = "qa_embeddings"
+#hr_elastic = hit_rate_elasticsearch(es, index_name, embedding_model, ground_truth, num_results=5)
+print(f"Hit-rate for Elasticsearch: {hr_elastic:.2f}")
