83648d5f533a34a345246b963359ac9e38279561
jankes2000 <jankes2000@interia.pl>
2024-07-11 11:53:55 +0200

    Implemented Question 4

--- 03-vector-search/03-homework.py
+++ 03-vector-search/03-homework.py
@@ -1,18 +1,52 @@
 from sentence_transformers import SentenceTransformer
 import requests 
 import numpy as np
-from vector_search_engine import VectorSearchEngine  # Import klasy
+import pandas as pd
+from vector_search_engine import VectorSearchEngine  
+from tqdm.auto import tqdm
+from elasticsearch import Elasticsearch
+# Import klasy
 
 
-# class VectorSearchEngine():
-#     def __init__(self, documents, embeddings):
-#         self.documents = documents
-#         self.embeddings = embeddings
+def evaluate(ground_truth, search_function):
+    relevance_total = []
 
-#     def search(self, v_query, num_results=10):
-#         scores = self.embeddings.dot(v_query)
-#         idx = np.argsort(-scores)[:num_results]
-#         return [self.documents[i] for i in idx]
+    for q in tqdm(ground_truth):
+        doc_id = q['document']
+        results = search_function(q)
+        relevance = [d['id'] == doc_id for d in results]
+        relevance_total.append(relevance)
+
+    return {
+        'hit_rate': hit_rate(relevance_total),
+    }
+
+def hit_rate(relevance_total):
+    cnt = 0
+
+    for line in relevance_total:
+        if True in line:
+            cnt = cnt + 1
+
+    return cnt / len(relevance_total)
+
+def hit_rate2(search_engine, ground_truth, num_results=5):
+    hits = 0
+    
+    for item in ground_truth:
+        #print(item)
+        query = item['question']
+        true_document_id = item['document']
+        
+        v_query = embedding_model.encode(query)
+        results = search_engine.search(v_query, num_results=num_results)
+        
+        for result in results:
+            if result['id'] == true_document_id:
+                hits += 1
+                break
+    
+    return hits / len(ground_truth)
 
 
 model_name = "multi-qa-distilbert-cos-v1"
@@ -58,4 +92,26 @@ print(highest_score)
 search_engine = VectorSearchEngine(documents=documents_raw, embeddings=X)
 search_result = search_engine.search(v, num_results=5)
 
-print(search_result)
+#print(search_result)
+
+base_url = 'https://github.com/DataTalksClub/llm-zoomcamp/blob/main'
+relative_url = '03-vector-search/eval/ground-truth-data.csv'
+ground_truth_url = f'{base_url}/{relative_url}?raw=1'
+
+df_ground_truth = pd.read_csv(ground_truth_url)
+df_ground_truth = df_ground_truth[df_ground_truth.course == 'machine-learning-zoomcamp']
+ground_truth = df_ground_truth.to_dict(orient='records')
+
+
+relevance_total = []
+
+# for q in tqdm(ground_truth):
+#     doc_id = q['question']
+#     relevance = [d['id'] == doc_id for d in search_result]
+#     relevance_total.append(relevance)
+
+hr = hit_rate2(search_engine, ground_truth, num_results=5)
+print(hr)
+
+# print(relevance_total)
+# print(hit_rate(relevance_total))
